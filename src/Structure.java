908e23750f0da030c9198c82667275c327b825ed
Alex <Alex@10.15.149.214>
2016-11-09 11:29:15 -0800

    Added the Structure class and the sprite class

---
+++ src/Structure.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+
+/**
+ * @author Alex
+ *
+ */
+public class Structure {
+	protected int x; 
+	protected int y;
+	protected Sprite sprite;
+	private boolean destroyed;
+	
+	
+	/**
+ 	 * @param x: x location of the structure
+	 * @param y: y location of the structure
+	 * @param loc: image location of the sprite
+	 */
+	public Structure(int x, int y, String loc){
+		destroyed = false;
+		this.x = x;
+		this.y = y;
+		//this.sprite = 
+		
+	}
+	
+	public void Draw(){
+		sprite.draw(x, y);
+	}
+	
+	
+	/**
+	 * Sets the structure state to destroyed
+	 */
+	public void destroy(){
+		destroyed = true;
+	}
+	
+	
+	/**
+	 * Resets the structure state to not destroyed
+	 */
+	public void reset(){
+		destroyed = false;
+	}
+	
+	
+	/**
+	 * 
+	 * @return the current structure state
+	 */
+	public boolean isDestoryed(){
+		return destroyed;
+	}
+}
