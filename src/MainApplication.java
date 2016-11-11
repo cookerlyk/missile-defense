065176cfd19d87443a8b8d64efbcf7258bde6b93
Alex <a_lee89@u.pacific.edu>
2016-11-10 16:10:31 -0800

    Added menu buttons and background image placeholders

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -4,8 +4,8 @@ import acm.graphics.*;
 import acm.program.*;
 
 public class MainApplication extends GraphicsApplication {
-	public static final int WINDOW_WIDTH = 800;
-	public static final int WINDOW_HEIGHT = 600;
+	public static final int WINDOW_WIDTH = 1024;
+	public static final int WINDOW_HEIGHT = 768;
 	
 	private SomePane somePane;
 	private MenuPane menu;
