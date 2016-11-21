74fc5276f430bacecc5ff939d1e62db770f5f155
Alex <a_lee89@u.pacific.edu>
2016-11-21 11:46:00 -0800

    Turret delay added

--- src/Turret.java
+++ src/Turret.java
@@ -1,11 +1,22 @@
 import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+import javax.swing.*;
 
-public class Turret extends Structure{
-	private int ammo = 10;
+public class Turret extends Structure implements ActionListener{
 	
+	private static int TIMERTICK = 1000;
+	private static int DELAYTIME = 2000;
+	private static int SHOTDELAY = 1000;
+	
+	private int ammo = 10;
+	private Timer timer;
+	private int fireDelay;
 	
 	public Turret(MainApplication app, int x, int y, String loc){
 		super(app, x, y, loc);
+		run();
 	}
 	
 	/**
@@ -13,7 +24,7 @@ public class Turret extends Structure{
 	 * @return is the turret able to fire a missile
 	 */
 	boolean canFire(){
-		if(ammo > 0 && this.destroyed == false){
+		if(ammo > 0 && this.destroyed == false && fireDelay <= 0){
 			return true;
 		}
 		else return false;
@@ -27,10 +38,24 @@ public class Turret extends Structure{
 	void fireMissile(MainApplication app, Level lvl, int x, int y, List<Missile> m){
 		if(canFire()){
 			ammo -=1;
-			
+			fireDelay = DELAYTIME;
 			Missile missile = new Missile("Sprites/Missile_placeholder.png", true, this.x, this.y, app, x, y);
 			m.add(missile);
 		}
 		
+	}
+	
+	public void run(){
+		timer = new Timer(TIMERTICK, this);
+		timer.setInitialDelay(0);
+		timer.start();
+		System.out.println(fireDelay);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		if (fireDelay > 0){
+			fireDelay -=SHOTDELAY;
+		}
+		System.out.println(fireDelay);
 	}
 }
