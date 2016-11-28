b36ca22c93e5d1d5c93e98e832112db421aeabdb
Alex <a_lee89@u.pacific.edu>
2016-11-27 17:28:40 -0800

    Rotated Placeholder Images for Friendly Missiles

--- src/Sprite.java
+++ src/Sprite.java
@@ -1,9 +1,22 @@
 import java.awt.Image;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
 import acm.graphics.GImage;
+import acm.graphics.GPolygon;
 
 public class Sprite {
 	private GImage image;
+	private Image rotatedImage;
 	
 	public Sprite(GImage image) {
 		this.image = image;
@@ -13,6 +26,7 @@ public class Sprite {
 		this.image = image;
 		this.image.scale(xScale, yScale);
 	}
+	
 	
 	public void scale(double xScale, double yScale){
 		this.image.scale(xScale, yScale);
